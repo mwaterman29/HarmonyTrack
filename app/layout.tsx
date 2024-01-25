650c7902fcdd804999d115f9f31a6515c053e165
Matt <mwaterman29@gmail.com>
2024-01-25 13:40:55 -0500

    redone navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextAuthProvider } from "@/components/NextAuthProvider";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,9 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <NextAuthProvider>
         <body className={inter.className}>
-            <Header />
             {children}
-            <Footer />
         </body>
       </NextAuthProvider>
     </html>
---
+++ app/track/feelings/page.tsx
@@ -0,0 +1,30 @@
+import FeelingsWheel from "@/components/FeelingsWheel";
+import FeelingsInput from "@/components/FeelingsInput";
+import FeelingsEmoji from "@/components/FeelingsEmoji";
+
+import Image from "next/image";
+import UserControl from "@/components/UserControl";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../api/auth/[...nextauth]/options";
+import { FeelingStyle } from "@prisma/client";
+
+export default async function Home() {
+
+  const session = await getServerSession(authOptions);
+  const style = session.user.FeelingStyle as FeelingStyle;
+
+  //console.log(session);
+
+  return (
+    <div className='h-screen'>
+      <div className='flex flex-col bg-gray-950 h-full w-full p-6'>
+        <p className="p-2 pb-6 w-full text-center text-2xl">How Are You Today?</p>
+        <div className='flex h-full max-h-[640px]'>
+          {style == FeelingStyle.WHEEL && <FeelingsWheel />}
+          {style == FeelingStyle.EMOJI && <FeelingsEmoji />}
+          {style == FeelingStyle.INPUT && <FeelingsInput />}
+        </div>
+      </div>
+    </div>
+  );
+}
