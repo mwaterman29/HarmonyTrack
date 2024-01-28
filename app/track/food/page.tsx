44479d1258fb70fb870f6bfcd7a442499a278d9e
Matt Waterman <mwaterman29@gmail.com>
2024-01-28 00:17:03 -0500

    usable basic layout

--- app/track/food/page.tsx
+++ app/track/food/page.tsx
@@ -14,37 +14,59 @@ const TrackFood = () =>
 
 
     return (
-        <div className="text-black">
-            <p>Track Eating and Calories</p>
-            <input type="checkbox" checked={autoCalculateCals} onChange={() => setAutoCalculateCals(!autoCalculateCals)} />
+        <div className="text-white p-4">
+            <p className="text-3xl">Track Eating and Calories</p>
 
-            <input type="number" placeholder="Calories" value={macros.calories} onChange={(e) => {
+            <div className="flex flex-row px-4">
+            <input type="checkbox" checked={autoCalculateCals} onChange={() => setAutoCalculateCals(!autoCalculateCals)} />
+            <p className="pl-4">Auto Calculate Calories</p>
+            </div>
+
+
+            <div className="flex flex-row p-4">
+                <p className="pr-4 w-32 text-white text-lg">Calories: </p>
+                <input className='text-black' type="number" placeholder="Calories" value={macros.calories} onChange={(e) => {
                 setMacros({
                     ...macros,
                     calories: parseInt(e.target.value),
                 })
             }}/>
-            <input type="number" placeholder="Protein" value={macros.protein} onChange={(e) => {
+            </div>
+            <div className="flex flex-row p-4">
+                <p className="pr-4 w-32 text-white text-lg">- Protein: </p>
+                <input className='text-black' type="number" placeholder="Protein" value={macros.protein} onChange={(e) => {
                 setMacros({
                     ...macros,
                     protein: parseInt(e.target.value),
                     calories: autoCalculateCals ? macros.carbs * 4 + parseInt(e.target.value) * 4 + macros.fat * 7 : macros.calories,
                 })
             }}/>
-            <input type="number" placeholder="Carbs" value={macros.carbs} onChange={(e) => {
+            </div>
+            <div className="flex flex-row p-4">
+                <p className="pr-4 w-32 text-white text-lg">- Carbs: </p>
+                <input className='text-black'  type="number" placeholder="Carbs" value={macros.carbs} onChange={(e) => {
                 setMacros({
                     ...macros,
                     carbs: parseInt(e.target.value),
                     calories: autoCalculateCals ? parseInt(e.target.value) * 4 + macros.protein * 4 + macros.fat * 7 : macros.calories,
                 })
             }}/>
-            <input type="number" placeholder="Fat" value={macros.fat} onChange={(e) => {
+            </div>
+            <div className="flex flex-row p-4">
+                <p className="pr-4 w-32 text-white text-lg">- Fat: </p>
+                <input className='text-black'  type="number" placeholder="Fat" value={macros.fat} onChange={(e) => {
                 setMacros({
                     ...macros,
                     fat: parseInt(e.target.value),
                     calories: autoCalculateCals ? macros.carbs * 4 + macros.protein * 4 + parseInt(e.target.value) * 7 : macros.calories,
                 })
             }}/>
+            </div>
+
+
+
+
+
 
         </div>
     )
