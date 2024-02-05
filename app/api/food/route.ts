0ff7b456f85b1cb135daef9deeb8d26ae657a7c6
Matt Waterman <mwaterman29@gmail.com>
2024-02-05 15:38:38 -0500

    add and return foods

--- app/api/food/route.ts
+++ app/api/food/route.ts
@@ -2,10 +2,38 @@ import { getServerSession } from "next-auth/next"
 import { authOptions } from "../auth/[...nextauth]/options";
 import prisma from "@/app/prisma";
 
+const getFood = async (request: Request) => {
+    const session = await getServerSession(authOptions);
 
-export const createOrUpdateEatingDay = async (request: Request) => {
+    if(!session?.user)
+    {
+        return new Response("Unauthorized.",
+        {
+            status: 401,
+        });
+    }
+
+    const foods = await prisma.food.findMany({
+        where: {
+            userId: session.user.id as string,
+        },
+        orderBy: {
+            lastUsed: 'asc',
+        },
+    });
+
+    return new Response(JSON.stringify(foods),
+    {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
+
+const createOrUpdateFood = async (request: Request) => {
     const body = await request.json()
-    const { name, fat, carbs, protein, calories } = body;
+    const { id, name, fat, carbs, protein, calories } = body;
     const session = await getServerSession(authOptions);
 
     if(!session?.user)
@@ -18,29 +46,68 @@ export const createOrUpdateEatingDay = async (request: Request) => {
 
     let res;
 
-    try {
-        await prisma.food.create({
-            data: {
-                userId: session.user.id as string,
-                name,
-                fat,
-                carbs,
-                protein,
-                calories,
-            },
-        });
+    //Update an existing food entry
+    if(id)
+    {
+        try {
+            await prisma.food.update({
+                where: {
+                    id: id,
+                    userId: session.user.id as string,
+                },
+                data: {
+                    name,
+                    fat,
+                    carbs,
+                    protein,
+                    calories,
+                },
+            });
 
-        res = new Response("Resource Created",
-        {
-            status: 201,
-        });
-    } catch (error) {
-        console.error('Error creating or updating eating day entry:', error);
-        res = new Response("Internal Server Error",
-        {
-            status: 500,
-        });
+            res = new Response("Resource Updated",
+            {
+                status: 200,
+            });
+        } catch (error) {
+            console.error('Error creating or updating food entry:', error);
+            res = new Response("Internal Server Error",
+            {
+                status: 500,
+            });
+        }
+
+        return res;
     }
-
-    return res;
+    //Otherwise, create a new food entry
+    else
+    {
+        try {
+            const newFood = await prisma.food.create({
+                data: {
+                    userId: session.user.id as string,
+                    name,
+                    fat,
+                    carbs,
+                    protein,
+                    calories,
+                },
+            });
+    
+            res = new Response(JSON.stringify(newFood),
+            {
+                status: 201,
+                statusText: "Resource Created",
+            });
+        } catch (error) {
+            console.error('Error creating or updating eating day entry:', error);
+            res = new Response("Internal Server Error",
+            {
+                status: 500,
+            });
+        }
+    
+        return res;
+    }
 };
+
+export { getFood as GET, createOrUpdateFood as POST}
--- app/track/food/page.tsx
+++ app/track/food/page.tsx
@@ -15,6 +15,14 @@ const TrackFood = () =>
         fat: 0,
     });
 
+    const [addFoodMacros, setAddFoodMacros] = useState({
+        calories: 0,
+        protein: 0,
+        carbs: 0,
+        fat: 0,
+    });
+    const [addFoodName, setAddFoodName] = useState('');
+
     const { data: session, status } = useSession()
 
     useEffect(() => {
@@ -55,56 +63,128 @@ const TrackFood = () =>
             console.log(json);
         });
     }
+    
+    function submitAddFood()
+    {
+        fetch('/api/food', {
+            method: 'POST',
+            body: JSON.stringify({
+                date: new Date(),
+                name: addFoodName,
+                calories: addFoodMacros.calories,
+                protein: addFoodMacros.protein,
+                carbs: addFoodMacros.carbs,
+                fat: addFoodMacros.fat,
+            })
+        }).then(async (response) => {
+            const json = await response.json();
+            console.log(json);
+        });
+    }
 
     return (
         <div className="text-white p-4">
             <p className="text-3xl">Track Eating and Calories</p>
 
-            <div className="flex flex-row px-4">
-            <input type="checkbox" checked={autoCalculateCals} onChange={() => setAutoCalculateCals(!autoCalculateCals)} />
-            <p className="pl-4">Auto Calculate Calories</p>
+            <div className="flex flex-row">
+                <div className="flex flex-col">
+                    <div className="flex flex-row px-4">
+                    <input type="checkbox" checked={autoCalculateCals} onChange={() => setAutoCalculateCals(!autoCalculateCals)} />
+                    <p className="pl-4">Auto Calculate Calories</p>
+                    </div>
+
+                    <div className="flex flex-row p-4">
+                    <p className="pr-4 w-32 text-white text-lg">Calories: </p>
+                    <input className='text-black' type="number" placeholder="Calories" value={macros.calories} onChange={(e) => {
+                        setMacros({
+                            ...macros,
+                            calories: parseInt(e.target.value),
+                        })
+                        }}/>
+                    </div>
+                    <div className="flex flex-row p-4">
+                        <p className="pr-4 w-32 text-white text-lg">- Protein: </p>
+                        <input className='text-black' type="number" placeholder="Protein" value={macros.protein} onChange={(e) => {
+                        setMacros({
+                            ...macros,
+                            protein: parseInt(e.target.value),
+                            calories: autoCalculateCals ? macros.carbs * 4 + parseInt(e.target.value) * 4 + macros.fat * 9 : macros.calories,
+                        })
+                        }}/>
+                    </div>
+                    <div className="flex flex-row p-4">
+                        <p className="pr-4 w-32 text-white text-lg">- Carbs: </p>
+                        <input className='text-black'  type="number" placeholder="Carbs" value={macros.carbs} onChange={(e) => {
+                        setMacros({
+                            ...macros,
+                            carbs: parseInt(e.target.value),
+                            calories: autoCalculateCals ? parseInt(e.target.value) * 4 + macros.protein * 4 + macros.fat * 9 : macros.calories,
+                        })
+                        }}/>
+                    </div>
+                    <div className="flex flex-row p-4">
+                        <p className="pr-4 w-32 text-white text-lg">- Fat: </p>
+                        <input className='text-black'  type="number" placeholder="Fat" value={macros.fat} onChange={(e) => {
+                        setMacros({
+                            ...macros,
+                            fat: parseInt(e.target.value),
+                            calories: autoCalculateCals ? macros.carbs * 4 + macros.protein * 4 + parseInt(e.target.value) * 9 : macros.calories,
+                        })
+                        }}/>
+                    </div>
+                </div>
+
+                <div className="pl-4">
+                    <p>Add Foods</p>
+                    <div className="flex flex-col">
+                        <p>Add Food</p>
+                        <div className="flex flex-row py-1">
+                            <p className="pr-4">Food Name: </p>
+                            <input type="text" className="text-black" value={addFoodName} onChange={(e) => {setAddFoodName(e.target.value)}} />
+                            <p className="pr-4">Calories: </p>
+                            <input type="number" className="text-black" min={0} value={addFoodMacros.calories} onChange={(e) => {
+                            setAddFoodMacros({
+                                ...addFoodMacros,
+                                calories: parseInt(e.target.value),
+                            })
+                            }}/>
+                        </div>
+                        <div className="flex flex-row py-1">
+                            <p className="pr-4">Protein: </p>
+                            <input type="number" className="text-black" min={0} value={addFoodMacros.protein} onChange={(e) => {
+                            setAddFoodMacros({
+                                ...addFoodMacros,
+                                protein: parseInt(e.target.value),
+                                calories: autoCalculateCals ? addFoodMacros.carbs * 4 + parseInt(e.target.value) * 4 + addFoodMacros.fat * 9 : addFoodMacros.calories,
+                            })
+                            }}/>
+                            <p className="pr-4">Carbs: </p>
+                            <input type="number" className="text-black" min={0} value={addFoodMacros.carbs} onChange={(e) => {
+                            setAddFoodMacros({
+                                ...addFoodMacros,
+                                carbs: parseInt(e.target.value),
+                                calories: autoCalculateCals ? parseInt(e.target.value) * 4 + addFoodMacros.protein * 4 + addFoodMacros.fat * 9 : addFoodMacros.calories,
+                            })
+                            }}/>
+                            <p className="pr-4">Fat: </p>
+                            <input type="number" className="text-black" min={0} value={addFoodMacros.fat} onChange={(e) => {
+                            setAddFoodMacros({
+                                ...addFoodMacros,
+                                fat: parseInt(e.target.value),
+                                calories: autoCalculateCals ? addFoodMacros.carbs * 4 + addFoodMacros.protein * 4 + parseInt(e.target.value) * 9 : addFoodMacros.calories,
+                            })
+                            }}/>
+                        </div>
+                        <div className="flex items-center justify-center p-2">
+                            <button className="bg-blue-700 py-3 items-center rounded-md px-24" onClick={submitAddFood}>Add</button>
+                        </div>
+                    </div>
+
+                </div>
             </div>
 
 
-            <div className="flex flex-row p-4">
-                <p className="pr-4 w-32 text-white text-lg">Calories: </p>
-                <input className='text-black' type="number" placeholder="Calories" value={macros.calories} onChange={(e) => {
-                setMacros({
-                    ...macros,
-                    calories: parseInt(e.target.value),
-                })
-            }}/>
-            </div>
-            <div className="flex flex-row p-4">
-                <p className="pr-4 w-32 text-white text-lg">- Protein: </p>
-                <input className='text-black' type="number" placeholder="Protein" value={macros.protein} onChange={(e) => {
-                setMacros({
-                    ...macros,
-                    protein: parseInt(e.target.value),
-                    calories: autoCalculateCals ? macros.carbs * 4 + parseInt(e.target.value) * 4 + macros.fat * 9 : macros.calories,
-                })
-            }}/>
-            </div>
-            <div className="flex flex-row p-4">
-                <p className="pr-4 w-32 text-white text-lg">- Carbs: </p>
-                <input className='text-black'  type="number" placeholder="Carbs" value={macros.carbs} onChange={(e) => {
-                setMacros({
-                    ...macros,
-                    carbs: parseInt(e.target.value),
-                    calories: autoCalculateCals ? parseInt(e.target.value) * 4 + macros.protein * 4 + macros.fat * 9 : macros.calories,
-                })
-            }}/>
-            </div>
-            <div className="flex flex-row p-4">
-                <p className="pr-4 w-32 text-white text-lg">- Fat: </p>
-                <input className='text-black'  type="number" placeholder="Fat" value={macros.fat} onChange={(e) => {
-                setMacros({
-                    ...macros,
-                    fat: parseInt(e.target.value),
-                    calories: autoCalculateCals ? macros.carbs * 4 + macros.protein * 4 + parseInt(e.target.value) * 9 : macros.calories,
-                })
-            }}/>
-            </div>
+            
         </div>
     )
 }
